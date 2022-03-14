9b86d6edca15b551afe7383df6271fee021aa2d2
zhouhang <zhouhang@idataway.com>
2022-03-14 16:12:13 +0800

    update

--- setup.py
+++ setup.py
@@ -9,11 +9,14 @@ https://github.com/pypa/sampleproject
 from setuptools import setup, find_packages
 # To use a consistent encoding
 from codecs import open
-from os import path
+from os import path as os_path
+this_directory = os_path.abspath(os_path.dirname(__file__))
 
-# Get the long description from the README file
-with open('README.rst', encoding='utf-8') as f:
-    long_description = f.read()
+# 读取文件内容
+def read_file(filename):
+    with open(os_path.join(this_directory, filename), encoding='utf-8') as f:
+        long_description = f.read()
+    return long_description
 
 # Arguments marked as "Required" below must be included for upload to PyPI.
 # Fields marked as "Optional" may be commented out.
@@ -21,12 +24,14 @@ with open('README.rst', encoding='utf-8') as f:
 setup(
     # name='GeocodingCHN',
     name='GeocodingCHN',
-    version='1.2',
+    version='1.4.0',
     author='ZhouHang',
     author_email='fjkl@vip.qq.com',
     description='地址标准化',
-    long_description=long_description,
-    # long_description_content_type='text/markdown',  # 文档格式
+    long_description=read_file('README.md'),  # 读取的Readme文档内容
+    long_description_content_type="text/markdown",  # 指定包文档格式为markdown
+    license="MIT Licence",
+    url='https://github.com/casuallyName/Geocoding',
     packages=find_packages(),
     package_data={'GeocodingCHN': ['*.jar'],},
     include_package_data=True,
